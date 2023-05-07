1e7099751634587038dfde13f1b0497095ba9ece
Ste517 <66008197+Ste517@users.noreply.github.com>
2023-05-07 17:48:34 +0200

    commit 11

--- mysite/views.py
+++ mysite/views.py
@@ -3,7 +3,7 @@ from django.template import loader
 from .days import DateCalculator
 
 def index(request):
-    template = loader.get_template("templates/index.html")
+    template = loader.get_template("index.html")
     return HttpResponse(template.render(request))
 
 def schoolcountdown(request):
