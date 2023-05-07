ee6c45f426c07cc829a6263d9246b2be03f9a917
Ste517 <66008197+Ste517@users.noreply.github.com>
2023-05-07 17:37:20 +0200

    commit 9

--- mysite/views.py
+++ mysite/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
+from django.template import loader
 from .days import DateCalculator
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    template = loader.get_template("index.html")
+    return HttpResponse(template.render(request))
 
 def schoolcountdown(request):
     result = DateCalculator((2023,6,10),weekmask="1111110",holidays=["2023-06-10"]).calculate()
